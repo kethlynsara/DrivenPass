3a5886eb48f73d9f834188f9f1f98d4deb97c607
Kethlyn <kethlynsaraa@gmail.com>
2022-07-14 16:20:05 -0300

    feat: add validation in the file tokenMiddleware.ts

--- src/middlewares/tokenMiddleware.ts
+++ src/middlewares/tokenMiddleware.ts
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 import * as authRepository from "../repositories/authRepository.js";
 
@@ -12,8 +14,9 @@ export async function validateToken(req: Request, res: Response, next: NextFunct
         return res.status(401).send("invalid token");
     }
 
+    const data = jwt.verify(token, process.env.JWT_KEY);
     const session = await authRepository.findSession(token);
-    if (!token || !session) {
+    if (!token || !session || !data) {
         return res.status(401).send("invalid token");
     }
 
